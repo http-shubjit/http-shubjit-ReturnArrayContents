fc3ba40d6fd1af06f9eb335e88192d26b7c780a1
Shubhajit <shubjitgithub@gmail.com>
2024-06-07 07:06:20 +0530

    Done

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -31,7 +31,11 @@ public class Lab {
      * @param nums the array to be manipulated.
      * @return a string representation of all values of nums, with the values separated by spaces.
      */
-    public String arrayToString(int[] nums){
-        return "";
+    public String arrayToString(int[] nums) {
+        String str = "";
+        for (int i = 0; i < nums.length; i++) {
+            str = str + nums[i] + " ";
+        }
+        return str;
     }
 }
